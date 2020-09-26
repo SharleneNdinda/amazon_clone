2e46907c7a6bb79e7511a3ceab8299f18dd3d53c
Ndindar <sharlenendinda@gmail.com>
2020-09-26 18:19:00 +0300

    Top naivgation bar integration

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAy-GOhEZAfHo8YdUXv5GLaQjWLAUczv1Y",
+    authDomain: "clone-70938.firebaseapp.com",
+    databaseURL: "https://clone-70938.firebaseio.com",
+    projectId: "clone-70938",
+    storageBucket: "clone-70938.appspot.com",
+    messagingSenderId: "98681030300",
+    appId: "1:98681030300:web:84d368cbf85ec3b866cf59",
+    measurementId: "G-GW7RQ86L6L"
+  };
